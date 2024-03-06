0cc6beb76e7cd23d4ec225463e77b687fd60b0f8
EduHenriqs <110845651+EduHenriqs@users.noreply.github.com>
2024-03-05 22:05:38 -0300

    Add files via upload

--- rgb_hsv.php
+++ rgb_hsv.php
@@ -10,9 +10,7 @@
          if($_POST['hex'] != ''){
 
             $hex_formatado = substr($_POST['hex'], 1);
-           
             $hex_split = str_split($hex_formatado);
-            
             $red_bruto = $hex_split[0] . $hex_split[1];
             $green_bruto = $hex_split[2] . $hex_split[3];
             $blue_bruto = $hex_split[4] . $hex_split[5]; 
@@ -26,49 +24,53 @@
             $red = $_POST['red'];
             $green = $_POST['green'];
             $blue = $_POST['blue'];
-            
             $cores = array($_POST['red'], $_POST['green'], $_POST['blue']);
-
-           $hex_comp = sprintf("#%02x%02x%02x", $cores[0], $cores[1], $cores[2]);
+            $hex_comp = sprintf("#%02x%02x%02x", $cores[0], $cores[1], $cores[2]);
             
 
          }  
 
-        $r = $red/255;
-        $g = $green/255;
-        $b = $blue/255;
+        $r = number_format($red/255, 3, '.', '');
+        $g = number_format($green/255, 3, '.', '');
+        $b = number_format($blue/255, 3, '.', '');
+        //print $r . '<br>' . $g . '<br>' . $b;
+        $cmax = max($r,$g,$b);
+        $cmin = min($r,$g,$b);
+        $delta = $cmax - $cmin;
+         //print $delta;
+        if($delta < 0){
 
-        $cor_max = max($red, $green, $blue);
-        $cor_min = min($red, $green, $blue);
-
-        $delta = $cor_max - $cor_min;
-
-         $aux = ($cor_max + $cor_min) / 2;
-
-        if($delta == 0){
             $hue = 0;
-            $saturacao = 0;
-        }else{
 
-            $saturacao = $delta /    (1 - abs(2 * $aux - 1));    
+        }else if($cmax == $r){
+            
+            if ($g < $b) $somar = 360;
+            else if ($g >= $b) $soma = 0;            
 
-            switch ($cor_max) {
-                case $red:
-                    $hue = 60 * fmod(($green - $blue) / $delta, 6);
-                    break;
-                case $green:
-                    $hue = 60 * (($blue - $red) / $delta + 2);
-                    break;
-                case $blue:
-                    $hue = 60 * (($red - $geen) / $delta + 4);
-                    break;
+            $hue = 60*( ($g - $b)/$delta) + $somar;
+            //print $hue;
+        }else if($cmax == $g){
+
+            $hue = 60*((($b-$r)/$delta))+120;
+            //print $hue;
+        }else if($cmax == $b){
+
+            $hue = 60*((($r-$g)/$delta))+240;
+            //print $hue;
         }
 
-        $valor = $aux;
+        if($cmax == 0){
 
-    }
-    }
+            $saturacao = 0;
 
+        }else{
+
+            $saturacao = $delta/$cmax;
+
+        }
+
+        $value = $cmax;
+    }
 ?>
 <div class='row'>
     <div class='col-md-1'></div>
@@ -85,6 +87,7 @@
             <input type="text" class='form-control' name='blue' id='blue' value='<?= $blue?>'>
             <br>
             <button type="submit" class='btn btn-success'>Converter</button>
+            <button type="button" class='btn btn-danger' onclick='recarregar()'>Resetar</button>
         </form>
         <br>
         <h2>Result</h2>
@@ -93,23 +96,33 @@
 
 
         <label>Saturarion</label>
-        <input value="<?= $saturacao?>" class='form-control' disabled>
+        <input value="<?= number_format($saturacao*100,1,'.','')?>%" class='form-control' disabled>
 
 
         <label>Value</label>
-        <input value="<?= $valor?>" class='form-control' disabled>
+        <input value="<?= number_format($value*100,1,'.','')?>%" class='form-control' disabled>
 
 
         <label for="">Color Preview</label>
-        <input type="color" class="form-control form-control-color" value="<?= $hex_comp?>" disabled style='width:250px; height:250px;'>
+        <input type="color" class="form-control form-control-color" value="<?= $hex_comp?>" disabled style='width:250px; height:250px;'> 
 
 
     </div>
     <div class='col-md-1'></div>
 </div>
+<form name="limpa" id="limpa" method="post" action="<?= $_SERVER['php_self']?>">
 
-
-
-
+</form>
 </body>
 </html>
+
+
+<script>
+
+    function recarregar(){
+
+        document.limpa.submit();
+
+    }
+
+</script>
